b54501d9f9eac35d021fbd4a8e05fa88ba3b8c8f
Matthew Gilk <m@gilk.co>
2021-03-03 23:26:15 -0600

    weird comp

--- march_2_21/src/main.rs
+++ march_2_21/src/main.rs
@@ -10,11 +10,12 @@ struct Point {
     x: f32,
     y: f32,
     z: f32,
+    size: f32,
 }
 
 impl Point {
-    fn new(x: f32, y: f32, z: f32) -> Point {
-        Point { x, y, z }
+    fn new(x: f32, y: f32, z: f32, size: f32) -> Point {
+        Point { x, y, z, size }
     }
 }
 
@@ -31,7 +32,8 @@ fn model(app: &App) -> Model {
         .build()
         .unwrap();
 
-    let points = vec![Point::new(0.0, 0.0, 0.0)];
+    let mut points: Vec<Point> = Vec::new();
+    points.push(Point::new(0.0, 0.0, 0.0, 100.0));
 
     Model { _window, points }
 }
@@ -39,22 +41,33 @@ fn model(app: &App) -> Model {
 fn update(app: &App, model: &mut Model, _update: Update) {
     let secs = app.elapsed_frames() as f32 / 60.0;
 
+    if model.points.len() < 100 {
+        let p = model.points.last().unwrap();
+        model.points.push(Point::new(p.x, p.y, p.z, p.size));
+    }
+
+    let mut i = 0;
     for p in model.points.iter_mut() {
-        // p.x = secs.sin() * 100.0;
-        p.y = secs.sin() * 100.0;
+        p.y = (secs - (i as f32 * 0.1)).sin() * 100.0;
+        p.size = (secs - (i as f32 * 0.1)).sin() * 100.0; // i as f32;
+        i += 1;
     }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    if app.elapsed_frames() == 1 {
-        draw.background().color(BLACK);
-    }
-    draw.rect()
-        .w_h(SIZE as f32, SIZE as f32)
-        .color(srgba(0.0, 0.0, 0.0, 0.1));
+    // if app.elapsed_frames() == 1 {
+    draw.background().color(BLACK);
+    // }
+    // draw.rect()
+    //     .w_h(SIZE as f32, SIZE as f32)
+    //     .color(srgba(0.0, 0.0, 0.0, 0.1));
     for p in model.points.iter() {
-        draw.ellipse().x_y(p.x, p.y).color(WHITE);
+        draw.ellipse()
+            .w_h(p.size, p.size)
+            .z(p.size)
+            .x_y(p.size, p.size)
+            .color(srgba(p.size / 100.0, 1.0, p.size / 100.0, p.size / 100.0));
     }
     draw.to_frame(app, &frame).unwrap();
 }
