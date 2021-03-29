d087eab6b95d5faf7fd4bc63e24ce7d7406666e9
Matthew Gilk <m@gilk.co>
2021-03-28 23:09:38 -0500

    march 28 2021

---
+++ march_28_21/src/main.rs
@@ -0,0 +1,40 @@
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model).update(update).run();
+}
+
+struct Model {
+    _window: window::Id,
+}
+
+fn model(app: &App) -> Model {
+    let _window = app.new_window().view(view).build().unwrap();
+    Model { _window }
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+const SIZE: f32 = 3.0;
+const SPACE: f32 = 10.0;
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let row = (app.window_rect().h() / (SIZE as f32 + SPACE * 2.0)) as i32;
+    let col = (app.window_rect().w() / (SIZE as f32 + SPACE * 2.0)) as i32;
+    let draw = app.draw();
+    let time = app.elapsed_frames() / 3;
+    draw.background().color(BLACK);
+
+    for i in -row..row + 1 {
+        for j in -col..col + 1 {
+            let rad = (time as f32 + i as f32 + (j as f32 + time as f32).sin()) / 4.0;
+            let scale = (rad / 2.0).sin();
+            draw.ellipse()
+                .color(WHITE)
+                .x_y(i as f32 * (SIZE + SPACE), j as f32 * (SIZE + SPACE))
+                .w_h(scale * SIZE, scale * SIZE);
+        }
+    }
+
+    draw.to_frame(app, &frame).unwrap();
+}
